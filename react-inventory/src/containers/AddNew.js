d41f4812fe4df26894f485ddf285bc49543e0a83
Suad Huskic <suadhuskic@yahoo.com>
2017-09-01 15:37:42 -0700

    formatting and refactor

--- react-inventory/src/containers/AddNew.js
+++ react-inventory/src/containers/AddNew.js
@@ -8,6 +8,12 @@ import { connect } from 'react-redux';
 import shortid from 'shortid';
 import validator from 'validator';
 
+//we get the inventory here because when they submit;
+//we check if the part number exists.
+//in the real world we would check this with a API request.
+//we keep the inventory object in the component state;
+//until we verify its okay okay to add/update to the store.
+//
 @connect((reduxStore) => {
   return {
     inventory: reduxStore.inventory.inventory
@@ -56,44 +62,44 @@ export default class AddNew extends Component {
   /**
   * event handler for when our form is submitted.
   *
-  * @param event - Proxy
+  * @param event - SyntheticEvent
   *
   * @return boolean
   */
   handleOnSubmit = (event) => {
+    //stop it.
     event.preventDefault();
 
+    //grab all the properties that are required and make sure they filled them out
     _.forIn(this.state.required, (value, key) => {
       if(value) {
         if(_.isEmpty(this.state.inventory[key])) {
-          this.appendError(key);
+          return this.appendError(key);
         }
       }
     });
 
-    if(!this.isFormValid()) {
-      //there is some errors in the queue; display.
-      return false;
-    }
     //if we made it here; we are all good;
     //now we can push to store.
     //need to assign a unique id using shortid
-    const newInventory = this.state.inventory;
+    const newInventory = _.assign(this.state.inventory);
     newInventory.id = shortid.generate();
 
     this.setState({
       inventory: newInventory
     });
 
+    //now we make a call to the store and add our new object.
     this.props.dispatch(inventory.add(this.state.inventory));
 
+    //now we can push back to the main page.
     this.props.history.push('/');
   }
 
   /**
   * handles the changing of an input field.
   *
-  * @param inputTarget - Proxy event.
+  * @param inputTarget -SyntheticEvent
   *
   * @return void;
   */
@@ -103,48 +109,53 @@ export default class AddNew extends Component {
       startedTyping: true
     });
 
+    const propertyName = inputTarget.target.name;
+    const propertyValue = inputTarget.target.value;
+
     //now we need to update our inventory object.
     const newInventory = _.assign(this.state.inventory);
-    newInventory[inputTarget.target.name] = inputTarget.target.value.trim()
+    newInventory[propertyName] = propertyValue;
     this.setState({
       inventory: newInventory
     });
 
-    if(!inputTarget.target.value) {
+    if(!propertyValue) {
       //no value. now check if its required.
-      if(this.state.required[inputTarget.target.name]) {
+      if(this.state.required[propertyName]) {
         //we need to append a error message for this.
-        return this.appendError(inputTarget.target.name);
+        return this.appendError(propertyName);
       }
     } else {
       //it has a value.
       //now are any rules for this specific input?
-      if(_.indexOf(['partNumber', 'qty'], inputTarget.target.name) !== -1) {
+      if(_.indexOf(['partNumber', 'qty'], propertyName) !== -1) {
         //test it.
-        if(!validator.isInt(inputTarget.target.value)) {
-          return this.appendError(inputTarget.target.name, "Needs to be a valid number.");
+        if(!validator.isInt(propertyValue)) {
+          return this.appendError(propertyName, "Needs to be a valid number.");
         }
       }
       //if its a money field.
-      if(_.indexOf(['cost'], inputTarget.target.name) !== -1) {
-        if(!validator.isCurrency(inputTarget.target.value)) {
-          return this.appendError(inputTarget.target.name, "Needs to be a valid money!.");
+      if(_.indexOf(['cost'], propertyName) !== -1) {
+        if(!validator.isCurrency(propertyValue)) {
+          return this.appendError(propertyName, "Needs to be a valid money!.");
         }
       }
 
       //check if there is already an error for this input and remove it.
-      if(this.getErrorByIndex(inputTarget.target.name)) {
-        this.removeError(inputTarget.target.name);
+      if(this.getErrorByIndex(propertyName)) {
+        this.removeError(propertyName);
       }
     }
     //now we check if it already exists.
-    const duplicate = this.props.inventory.find( (inventory) => {
-      return parseInt(inventory.partNumber, 10) === parseInt(this.state.inventory.partNumber, 10);
-    });
-    console.log(duplicate);
-    if(duplicate) {
-      return this.appendError(inputTarget.target.name, "Inventory already exists with part number: "+duplicate.partNumber);
+    const inventoryPartNumber = parseInt(this.state.inventory.partNumber, 10);
+
+    if(_.findIndex(this.props.inventory, (inventory) => {
+      console.log(parseInt(inventory.partNumber, 10));
+      return parseInt(inventory.partNumber, 10) === inventoryPartNumber;
+    }) !== -1) {
+      return this.appendError(propertyName, "Inventory already exists with part number: "+inventoryPartNumber);
     }
+    return true;
   }
 
   /**
